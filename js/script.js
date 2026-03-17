e93cb7a6ad22efc6ce8c470a54eac469edeb314c
14HAK <dulonmolla88@gmail.com>
2026-03-18 05:55:32 +0600

    Calculator Evalution func() done. need to proper logic building

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('simple calculator app');
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.calc-btn');
+const clearButton = document.getElementById('clear');
+const equalButton = document.getElementById('equals');
+
+let currentInput = '';
+
+buttons.forEach((button) => {
+	button.addEventListener('click', () => {
+		currentInput += button.textContent;
+		display.value = currentInput;
+	});
+});
+
+clearButton.addEventListener('click', () => {
+	currentInput = '';
+	display.value = '';
+});
+
+equalButton.addEventListener('click', () => {
+	try {
+		currentInput = Number(currentInput);
+		console.log(currentInput);
+		// display.value = currentInput;
+	} catch (error) {
+		display.value = 'Error';
+		currentInput = '';
+	}
+});
